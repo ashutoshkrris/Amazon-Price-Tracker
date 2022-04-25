5d4bd8bf87a0807641d8595347825be2db07b7ba
Ashutosh Krishna <ashutoshbritish@gmail.com>
2022-04-25 17:19:58 +0530

    Fix tracker

--- tracker.py
+++ tracker.py
@@ -15,6 +15,7 @@ import json
 from datetime import datetime
 import time
 
+
 class GenerateReport:
     def __init__(self, file_name, filters, base_link, currency, data):
         self.data = data
@@ -36,7 +37,6 @@ class GenerateReport:
             json.dump(report, file, indent=2)
         print("Report generated successfully..")
 
-
     def get_best_item(self):
         try:
             return sorted(self.data, key=lambda k: k['price'])[0]
@@ -44,6 +44,7 @@ class GenerateReport:
             print(e)
             print("Problem with sorting items")
             return None
+
 
 class AmazonAPI:
     def __init__(self, search_term, filters, base_url, currency):
@@ -53,7 +54,7 @@ class AmazonAPI:
         set_ignore_certificate_error(options)
         set_browser_as_incognito(options)
         self.driver = get_chrome_driver(options)
-           
+
         self.currency = currency
         self.price_filter = f"&rh=p_36%3A{filters['min']}00-{filters['max']}00"
 
@@ -75,19 +76,24 @@ class AmazonAPI:
         asins = self.get_asins(links)
         products = []
         for asin in asins:
-            product = self.get_single_product_info(asin)
+            product = self.get_single_product_info(asin, asins.index(asin))
             if product:
                 products.append(product)
+        print(f"Pr: {products}")
         return products
 
-    def get_single_product_info(self, asin):
-        print(f"Product ID: {asin} - getting data...")
+    def get_single_product_info(self, asin, index):
+        print(f"{index+1} Product ID: {asin} - getting data...")
         product_short_url = self.shorten_url(asin)
+        print(product_short_url)
         self.driver.get(f'{product_short_url}?language=en_IN')
         time.sleep(2)
         title = self.get_title()
+        print(f"Title: {title}")
         seller = self.get_seller()
+        print(f"Seller: {seller}")
         price = self.get_price()
+        print(f"Price: {price}")
         if title and seller and price:
             product_info = {
                 'asin': asin,
@@ -118,20 +124,21 @@ class AmazonAPI:
     def get_price(self):
         price = None
         try:
-            price = self.driver.find_element_by_id('priceblock_ourprice').text
-            if price == '':
-                price = self.driver.find_element_by_id('priceblock_dealprice')
+            price = self.driver.find_element_by_class_name('apexPriceToPay').text
             price = self.convert_price(price)
         except NoSuchElementException:
             try:
-                availability = self.driver.find_element_by_id('availability').text
+                availability = self.driver.find_element_by_id(
+                    'availability').text
                 if 'Available' in availability or 'In Stock' in availability:
-                    price = self.driver.find_element_by_class_name('olp-padding-right').text
+                    price = self.driver.find_element_by_class_name(
+                        'olp-padding-right').text
                     price = price[price.find(self.currency):]
                     price = self.convert_price(price)
             except Exception as e:
                 print(e)
-                print(f"Can't get price of a product - {self.driver.current_url}")
+                print(
+                    f"Can't get price of a product - {self.driver.current_url}")
                 return None
         except Exception as e:
             print(e)
@@ -143,19 +150,18 @@ class AmazonAPI:
         price = price.split(self.currency)[1]
         try:
             price = price.split("\n")[0] + "." + price.split("\n")[1]
-        except:
-            Exception()
+        except Exception:
+            pass
         try:
             price = price.split(",")[0] + price.split(",")[1]
-        except:
-            Exception()
+        except Exception:
+            pass
         return float(price)
-
 
     def get_asins(self, links):
         return [self.get_asin(link) for link in links]
 
-    def get_asin(self,product_link):
+    def get_asin(self, product_link):
         return product_link[product_link.find('/dp/') + 4:product_link.find('/ref')]
 
     def shorten_url(self, asin):
@@ -163,7 +169,7 @@ class AmazonAPI:
 
     def get_products_links(self):
         self.driver.get(self.base_url)
-        element = self.driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]')
+        element = self.driver.find_element_by_id("twotabsearchtextbox")
         element.send_keys(self.search_term)
         element.send_keys(Keys.ENTER)
         time.sleep(2)
@@ -171,14 +177,16 @@ class AmazonAPI:
         time.sleep(2)
         result_list = self.driver.find_elements_by_class_name('s-result-list')
         links = []
-        try :
-            results = result_list[0].find_elements_by_xpath('//div/span/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a')
+        try:
+            results = result_list[0].find_elements_by_xpath(
+                '//div/div/div/div/div/div[2]/div/div/div[1]/h2/a')
             links = [link.get_attribute('href') for link in results]
             return links
         except Exception as e:
             print(f"Couldn't find any product by name {self.search_term}...")
             print(e)
             return links
+
 
 if __name__ == "__main__":
     amazon = AmazonAPI(NAME, FILTERS, BASE_URL, CURRENCY)
